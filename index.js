4e323362c3c123b3f8e5b45b2b9ad4480aa1387f
Laviz-pand <pandeylaviz@gmail.com>
2023-10-25 15:23:04 +0545

    Array Remove

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function Add(){
-    
-}
+export const arrayRemove = (arr, item) => {
+  const i = arr.indexOf(item);
+  if (i === -1) {
+    return arr;
+  }
+  const result = arr.slice();
+  result.splice(i, 1);
+  return result;
+};
